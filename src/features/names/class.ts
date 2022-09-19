3888cc8969006db6274ac9ce7736f48dc52a3fa2
victor-heliomar <victordev2002@gmail.com>
2022-09-19 12:20:58 -0400

    draft: adding jsdoc

--- src/features/names/class.ts
+++ src/features/names/class.ts
@@ -5,11 +5,19 @@ export class Statistics {
   firstNames: NamesState = {};
   lastNames: NamesState = {};
 
-  static cardinality(object: NamesState) {
+  /**
+   * Get the cardinality of an object
+   * @param {NamesState} object
+  */
+  static cardinality(object: NamesState): number {
     return Object.keys(object).length;
   }
 
-  static top10(object: NamesState) {
+  /**
+   * Receive an object whose values are numbers, and return a sorted descending list with the top 10 bigger values
+   * @param {NamesState} object
+   */
+  static top10(object: NamesState): [string, number][] {
     return Object.entries(object)
       .sort(
         (
@@ -20,33 +28,57 @@ export class Statistics {
       .slice(0, 10);
   }
 
+  /**
+   * Create a new key in the fullNames' state, with the number of times that the key have been added to the object
+   * @param {string} fullName 
+   */
   addFullName(fullName: string) {
     if (!this.fullNames[fullName]) this.fullNames[fullName] = 0;
     this.fullNames[fullName] += 1;
   }
 
+  /**
+   * Create a new key in the firstNames' state, with the number of times that the key have been added to the object
+   * @param {string} firstName 
+   */
   addFirstName(firstName: string) {
     if (!this.firstNames[firstName]) this.firstNames[firstName] = 0;
     this.firstNames[firstName] += 1;
   }
 
+  /**
+   * Create a new key in the lastNames' state, with the number of times that the key have been added to the object
+   * @param {string} firstName 
+   */
   addLastName(lastName: string) {
     if (!this.lastNames[lastName]) this.lastNames[lastName] = 0;
     this.lastNames[lastName] += 1;
   }
 
+  /**
+   * Get the cardinality of fullNames object
+   */
   get uniqueFullNamesCount() {
     return Statistics.cardinality(this.fullNames);
   }
 
+  /**
+   * Get the cardinality of firstNames object
+   */
   get uniqueFirstNamesCount() {
     return Statistics.cardinality(this.firstNames);
   }
 
+  /**
+   * Get the cardinality of lastNames object
+   */
   get uniqueLastNamesCount() {
     return Statistics.cardinality(this.lastNames);
   }
 
+  /**
+   * Get a descending list the top10 more used lastNames
+   */
   get top10LastNames() {
     return Statistics.top10(this.lastNames);
   }
