d73a036ad3f097b2b7e332238c76c6fe53fdcc4f
Israel Antonio Rosales Laguan <israellaguan@gmail.com>
2022-09-19 03:19:37 -0500

    feat: add error management

--- src/features/files/index.ts
+++ src/features/files/index.ts
@@ -1,14 +1,12 @@
 import fs from "fs";
 import readline from "readline";
 import { Interface } from "readline";
-import { writeStatisticsToFile } from "../../interface/output";
-import { Statistics } from "../names";
 
 const writeFile = async (filePath: fs.PathLike, output: string) => {
   try {
     await fs.promises.writeFile(filePath, output);
   } catch (error) {
-    console.error;
+    console.error(error);
   }
 };
 
@@ -26,14 +24,18 @@ const lineReader = (
   onLine: (line: string) => void,
 ): Promise<Interface> => {
   return new Promise((resolve, reject) => {  
-    const lineReader = readline.createInterface({
-      input: readFile(filePath),
-    });
-    
-    lineReader.on("line", onLine);
-    lineReader.on("close", async function () {
-      resolve(lineReader);
-    });
+    try {
+      const lineReader = readline.createInterface({
+        input: readFile(filePath),
+      });      
+      lineReader.on("line", onLine);
+      lineReader.on("close", async function () {
+        resolve(lineReader);
+      });
+    } catch (error) {
+      console.error(error)
+      reject(error)
+    }
   })
 };
 
