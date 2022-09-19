8191928b80cbe8a13672b64a5dcd9ad186517863
Israel Antonio Rosales Laguan <israellaguan@gmail.com>
2022-09-19 10:02:37 -0500

    fix: improve cli message

--- src/interface/cli/index.ts
+++ src/interface/cli/index.ts
@@ -1,16 +1,12 @@
 const getHelpText = () => {
   const helpText = `
-  simplecli is a simple cli program to demonstrate how to handle files using streams.
+  code-challenge-1242 is a simple cli program to demonstrate how to handle files using streams.
   usage:
-      mycliprogram <command> <path_to_file>
+      code-challenge-1242 <path_to_file> [N=25]
 
-      <command> can be:
-      read: Print a file's contents to the terminal
-      write: Write a message from the terminal to a file
-      copy: Create a copy of a file in the current directory
-      reverse: Reverse the content of a file and save its output to another file.
+      <path_to_file> is the path to the input file you want to work with.
+      N is the number of Unique names to output, defaults to 25
 
-      <path_to_file> is the path to the file you want to work with.
   `;
   console.log(helpText);
 };
@@ -19,12 +15,12 @@ const checkArgs = (args: string[]) => {
   if (args.length < 3) {
     getHelpText();
     process.exit(0);
-  } else if (args.length > 4) {
+  } else if (args.length > 3) {
     console.log("More arguments provided than expected");
     getHelpText();
     process.exit(0);
   } else {
-    if (!args[3]) {
+    if (!args[2]) {
       console.log("This tool requires at least one path to a file");
       getHelpText();
       process.exit(0);
